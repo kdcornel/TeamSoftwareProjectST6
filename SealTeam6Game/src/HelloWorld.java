e04d223c2e19d4d1cc2db1892ae68aa1cbe9dc45
jrjaskol <43866680+jrjaskol@users.noreply.github.com>
2019-03-08 11:56:54 -0500

    Added Movement and can now change screen size
    Added some action listener-type things so the goomba can move around and also added a configuration so that we can change the size of the screen, looks quite a bit nicer. Tried to implement physics but I don't think 2DBox is installed on my system so ill look into that next.

--- SealTeam6Game/src/HelloWorld.java
+++ SealTeam6Game/src/HelloWorld.java
@@ -1,9 +1,12 @@
-//HI
+
+
 import java.util.Random;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,20 +15,27 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class HelloWorld extends ApplicationAdapter {
-
-    public static void main(String[] args) {
-        new LwjglApplication(new HelloWorld()); 
-    }
-
+	
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.width = 1800;
+		cfg.height = 900;
+		LwjglApplication app = new LwjglApplication(new HelloWorld(), cfg); 
+	}
+	
     private SpriteBatch batch;
     private BitmapFont font;
-
+    //World world;
+    //Body body;
+    
     @Override
     public void create() {        
         batch = new SpriteBatch();    
         font = new BitmapFont();
-        font.setColor(Color.BLACK);
         
+        //world = new World(new Vector2(0, -98f), true);
+        //World doesnt currently work, ill look into it
+        font.setColor(Color.BLUE);
     }
 
     @Override
@@ -33,20 +43,39 @@ public class HelloWorld extends ApplicationAdapter {
         batch.dispose();
         font.dispose();
     }
-
+    
+    //Speed for key press
+    float goombaSpeed = 150.0f;
+    float goombaX;
+    float goombaY;
+    
     @Override
-    public void render() {        
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+    public void render() {  
+    	
+    	//Action Listeners for dpad key presses
+    	if(Gdx.input.isKeyPressed(Keys.DPAD_UP))
+    		goombaY += Gdx.graphics.getDeltaTime() * goombaSpeed;       
+    	if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN))
+    		goombaY -= Gdx.graphics.getDeltaTime() * goombaSpeed;
+    	if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT))
+    		goombaX -= Gdx.graphics.getDeltaTime() * goombaSpeed;
+    	if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT))
+    		goombaX += Gdx.graphics.getDeltaTime() * goombaSpeed;
+    	
+    	    	
+    	Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        Texture texture = new Texture(Gdx.files.internal("C:/Users/joshj/workspace/SealTeam6Game/src/Goomba.png"));
-        Sprite sprite = new Sprite(texture, 10, 10, 400, 400);
-        sprite.setPosition(80, 25);
+        Texture texture = new Texture(Gdx.files.internal("C:/Users/joshj/workspace/GdxTestOrSomethingYouKnowThisIsReallyDumbBlahBlahBlahGottaMakeTheTitleLongerOrElseItsNotLikeProfessionalOrSomethingImDrunkDontJudgeMe/src/Goomba.png"));
+        Sprite sprite = new Sprite(texture);
         
-        batch.begin();        
-        sprite.draw(batch);
+      
+        batch.begin();  
+        batch.draw(texture, (int)goombaX, (int)goombaY);
+        //sprite.draw(batch);
         batch.end();
     }
+    
+
 
     @Override
     public void resize(int width, int height) {
@@ -59,4 +88,4 @@ public class HelloWorld extends ApplicationAdapter {
     @Override
     public void resume() {
     }
-} 
+}
