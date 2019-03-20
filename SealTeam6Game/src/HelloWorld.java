cae1401f3775dc339d95d134c65d26a976c7aa1a
jrjaskol <43866680+jrjaskol@users.noreply.github.com>
2019-03-20 11:56:48 -0400

    Background works with no lag
    Needed to use lower-res background to prevent lag

--- SealTeam6Game/src/HelloWorld.java
+++ SealTeam6Game/src/HelloWorld.java
@@ -20,8 +20,8 @@ public class HelloWorld extends ApplicationAdapter {
     
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.width = 400;
-		cfg.height = 350;
+		cfg.width = 1000;
+		cfg.height = 500;
 		LwjglApplication app = new LwjglApplication(new HelloWorld(), cfg); 
 	}
 	
@@ -114,13 +114,13 @@ public class HelloWorld extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
        
         Texture texture = new Texture(Gdx.files.internal("Assets/Goomba.png"));
-        //Texture bkgTexture = new Texture(Gdx.files.internal("Assets/background.png"));
+        Texture bkgTexture = new Texture(Gdx.files.internal("Assets/LowerResBkg.jpg"));
+        TextureRegion region = new TextureRegion(bkgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Sprite sprite = new Sprite(texture);
-        //Sprite bkgSprite = new Sprite(bkgTexture);
       
         batch.begin();  
+        batch.draw(region, 0,0);
         batch.draw(getTexture(currentAnim), (int)goombaX, (int)goombaY+10);
-        //sprite.draw(batch);
         batch.end();
         
         
